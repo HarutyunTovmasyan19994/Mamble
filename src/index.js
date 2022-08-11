f1b9f9b6ee3713e13b50e9fa3ed9dfce3f139e80
Harutyun <91012818+HarutyunTovmasyan19994@users.noreply.github.com>
2022-08-11 17:59:12 +0400

    Create Redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {PersistGate} from 'redux-persist/integration/react'
+import {Provider} from 'react-redux'
+import {store,Storage} from "./Redux/Store/store";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+        <PersistGate loading={false} persistor={Storage}>
+            <App/>
+        </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
